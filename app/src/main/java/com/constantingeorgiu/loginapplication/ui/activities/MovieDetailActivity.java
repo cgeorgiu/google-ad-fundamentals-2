9ba9e1d0042acb78c5426ef3f98dd9a0ceda053f
Constantin Georgiu <constantingeorgiu90@gmail.com>
2018-12-12 22:13:59 +0200

    - Reused code

--- app/src/main/java/com/constantingeorgiu/loginapplication/ui/activities/MovieDetailActivity.java
+++ app/src/main/java/com/constantingeorgiu/loginapplication/ui/activities/MovieDetailActivity.java
@@ -1,126 +1,32 @@
 package com.constantingeorgiu.loginapplication.ui.activities;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.RatingBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.constantingeorgiu.loginapplication.Constants;
 import com.constantingeorgiu.loginapplication.R;
-import com.constantingeorgiu.loginapplication.utils.ImageUtils;
-
-import java.util.Locale;
+import com.constantingeorgiu.loginapplication.ui.fragments.MovieDetailFragment;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
     private static final String TAG = MovieDetailActivity.class.getSimpleName();
-    private TextView tvMovieLink;
-    private TextView tvMovieName;
-    private TextView tvGenre;
-    private TextView tvActors;
-    private TextView tvYear;
-    private TextView tvDescription;
-    private ImageView ivMoviePoster;
-    private RatingBar rbMovieRating;
-
-    private String movieName = "";
-    private String movieGenre = "";
-    private String moviePhotoBase64 = "";
-    private String movieActors = "";
-    private float movieRating = 0;
-    private String movieDescription = "";
-    private int movieYear = 0;
-    private String movieIMDBLink = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate() called");
+        //Get bundle from MovieListActivity
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
-            try {
-                movieName = bundle.getString(Constants.MOVIE_NAME);
-                movieGenre = bundle.getString(Constants.MOVIE_GENRE);
-                moviePhotoBase64 = bundle.getString(Constants.MOVIE_PHOTO);
-                movieRating = bundle.getFloat(Constants.MOVIE_RATING);
-                movieDescription = bundle.getString(Constants.MOVIE_DESCRIPTION);
-                movieYear = bundle.getInt(Constants.MOVIE_YEAR);
-                movieActors = bundle.getString(Constants.MOVIE_ACTORS);
-                movieIMDBLink = bundle.getString(Constants.MOVIE_IMDB_LINK);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                Toast.makeText(this, "Crashed and burned", Toast.LENGTH_LONG).show();
-            }
+            MovieDetailFragment fragment = new MovieDetailFragment();
+            fragment.setArguments(bundle);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        } else {
+            Toast.makeText(this, "No bundle was recieved", Toast.LENGTH_LONG).show();
         }
-
-        tvDescription = findViewById(R.id.tvDescription);
-        tvMovieName = findViewById(R.id.tvMovieName);
-        tvGenre = findViewById(R.id.tvGenre);
-        tvActors = findViewById(R.id.tvActors);
-        tvYear = findViewById(R.id.tvYear);
-        tvMovieLink = findViewById(R.id.tvMovieLink);
-        ivMoviePoster = findViewById(R.id.ivMoviePoster);
-        rbMovieRating = findViewById(R.id.rbMovieRating);
-
-        tvDescription.setText(movieDescription);
-        tvMovieName.setText(movieName);
-        tvGenre.setText(movieGenre);
-        tvYear.setText(String.format(Locale.ENGLISH, "%d", movieYear));
-        tvActors.setText(movieActors);
-        tvMovieLink.setText(movieIMDBLink);
-        rbMovieRating.setRating(movieRating);
-        ivMoviePoster.setImageBitmap(ImageUtils.decodeImageFromString(moviePhotoBase64));
-
-        tvMovieLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MovieDetailActivity.this, WebViewActivity.class));
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart() called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
     }
 }
--- app/src/main/java/com/constantingeorgiu/loginapplication/ui/activities/MovieListActivity.java
+++ app/src/main/java/com/constantingeorgiu/loginapplication/ui/activities/MovieListActivity.java
@@ -22,6 +22,7 @@ import com.constantingeorgiu.loginapplication.R;
 import com.constantingeorgiu.loginapplication.model.Movie;
 import com.constantingeorgiu.loginapplication.dummy.DummyContent;
 import com.constantingeorgiu.loginapplication.ui.adapters.AdapterItemClickListener;
+import com.constantingeorgiu.loginapplication.ui.adapters.MoviesAdapter;
 import com.constantingeorgiu.loginapplication.ui.fragments.MovieDetailFragment;
 import com.constantingeorgiu.loginapplication.utils.ImageUtils;
 
@@ -44,6 +45,7 @@ public class MovieListActivity extends AppCompatActivity {
      */
 
     private ArrayList<Movie> moviesArrayList;
+    private MoviesAdapter moviesAdapter;
     private boolean mTwoPane;
 
     @Override
@@ -64,7 +66,8 @@ public class MovieListActivity extends AppCompatActivity {
             }
         });
 
-        moviesArrayList = new DummyContent().getMoviesList();
+        DummyContent dummyContent = new DummyContent();
+        moviesArrayList = dummyContent.getMoviesList();
 
         if (findViewById(R.id.movie_detail_container) != null) {
             // The detail container view will be present only in the
@@ -74,101 +77,40 @@ public class MovieListActivity extends AppCompatActivity {
             mTwoPane = true;
         }
 
+        moviesAdapter = new MoviesAdapter(moviesArrayList, new AdapterItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Movie item = moviesArrayList.get(position);
+                Bundle arguments = new Bundle();
+                arguments.putString(Constants.MOVIE_NAME, item.getName());
+                arguments.putString(Constants.MOVIE_DESCRIPTION, item.getShortDescription());
+                arguments.putFloat(Constants.MOVIE_RATING, item.getRating());
+                arguments.putString(Constants.MOVIE_GENRE, item.getGenre());
+                arguments.putInt(Constants.MOVIE_YEAR, item.getYear());
+                arguments.putString(Constants.MOVIE_ACTORS, item.getActors());
+                arguments.putString(Constants.MOVIE_IMDB_LINK, item.getImdbURL());
+                arguments.putString(Constants.MOVIE_PHOTO, item.getPhotoBase64());
+
+                if (mTwoPane) {
+                    MovieDetailFragment fragment = new MovieDetailFragment();
+                    fragment.setArguments(arguments);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.movie_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, MovieDetailActivity.class);
+                    intent.putExtras(arguments);
+                    context.startActivity(intent);
+                }
+            }
+        });
         View recyclerView = findViewById(R.id.movie_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new MovieItemRecyclerViewAdapter(this, moviesArrayList, mTwoPane));
-    }
-
-    public static class MovieItemRecyclerViewAdapter extends RecyclerView.Adapter<MovieItemRecyclerViewAdapter.MovieViewHolder> {
-
-        private final MovieListActivity mParentActivity;
-        private final List<Movie> mMovies;
-        private final boolean mTwoPane;
-        private final AdapterItemClickListener mOnClickListener = new AdapterItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                Movie item = mMovies.get(position);
-                if (mTwoPane) {
-                    Bundle arguments = new Bundle();
-                    arguments.putString(Constants.MOVIE_NAME, item.getName());
-                    arguments.putString(Constants.MOVIE_DESCRIPTION, item.getShortDescription());
-                    arguments.putFloat(Constants.MOVIE_RATING, item.getRating());
-                    arguments.putString(Constants.MOVIE_GENRE, item.getGenre());
-                    arguments.putInt(Constants.MOVIE_YEAR, item.getYear());
-                    arguments.putString(Constants.MOVIE_ACTORS, item.getActors());
-                    arguments.putString(Constants.MOVIE_IMDB_LINK, item.getImdbURL());
-                    arguments.putString(Constants.MOVIE_PHOTO, item.getPhotoBase64());
-                    MovieDetailFragment fragment = new MovieDetailFragment();
-                    fragment.setArguments(arguments);
-                    mParentActivity.getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.movie_detail_container, fragment)
-                            .commit();
-                } else {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, MovieDetailActivity.class);
-                    intent.putExtra(Constants.MOVIE_NAME, item.getName());
-                    intent.putExtra(Constants.MOVIE_DESCRIPTION, item.getShortDescription());
-                    intent.putExtra(Constants.MOVIE_RATING, item.getRating());
-                    intent.putExtra(Constants.MOVIE_YEAR, item.getYear());
-                    intent.putExtra(Constants.MOVIE_GENRE, item.getGenre());
-                    intent.putExtra(Constants.MOVIE_ACTORS, item.getActors());
-                    intent.putExtra(Constants.MOVIE_IMDB_LINK, item.getImdbURL());
-                    intent.putExtra(Constants.MOVIE_PHOTO, item.getPhotoBase64());
-                    context.startActivity(intent);
-                }
-            }
-        };
-
-        MovieItemRecyclerViewAdapter(MovieListActivity parent, List<Movie> items, boolean twoPane) {
-            this.mMovies = items;
-            this.mParentActivity = parent;
-            this.mTwoPane = twoPane;
-        }
-
-        @Override
-        public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_movie, parent, false);
-            return new MovieViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final MovieViewHolder holder, final int position) {
-            Movie movie = mMovies.get(position);
-            holder.tvMovieName.setText(movie.getName());
-            holder.tvMovieGenre.setText(movie.getGenre());
-            holder.rbMovieRating.setRating(movie.getRating());
-            holder.ivMovieIcon.setImageBitmap(ImageUtils.decodeImageFromString(movie.getPhotoBase64()));
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mOnClickListener.onItemClick(v, position);
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return mMovies.size();
-        }
-
-        public class MovieViewHolder extends RecyclerView.ViewHolder {
-            // each data item is just a string in this case
-            public TextView tvMovieName;
-            public TextView tvMovieGenre;
-            public RatingBar rbMovieRating;
-            public ImageView ivMovieIcon;
-
-            public MovieViewHolder(View view) {
-                super(view);
-                tvMovieName = view.findViewById(R.id.tv_movieName);
-                tvMovieGenre = view.findViewById(R.id.tv_movieGenre);
-                rbMovieRating = view.findViewById(R.id.rb_movieRating);
-                ivMovieIcon = view.findViewById(R.id.iv_movieIcon);
-            }
-        }
+        recyclerView.setAdapter(moviesAdapter);
     }
 }
