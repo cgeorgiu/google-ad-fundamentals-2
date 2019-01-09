a4dd4fef77bc143291ee84e237dafa5adac2c519
Constantin Georgiu <constantingeorgiu90@gmail.com>
2019-01-09 20:09:30 +0200

    Added firebase auth login mechanism

--- app/src/main/java/com/constantingeorgiu/loginapplication/ui/activities/LoginActivity.java
+++ app/src/main/java/com/constantingeorgiu/loginapplication/ui/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package com.constantingeorgiu.loginapplication.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,19 +10,26 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.constantingeorgiu.loginapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     private Button btnLogin;
     private EditText etUserName;
     private EditText etPassword;
-    private String registeredUser = "Constantin";
-    private String validPassword = "abcd1234";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
         etUserName = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
@@ -43,20 +51,17 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
 
-                if (!usernameFromForm.equals(registeredUser)) {
-                    etUserName.setError("Invalid username");
-                    etUserName.requestFocus();
-                    return;
-                }
-
-                if (!passwordFromForm.equals(validPassword)) {
-                    etPassword.setError("Invalid password");
-                    etPassword.requestFocus();
-                    return;
-                }
-
-                Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(LoginActivity.this, MovieListActivity.class));
+                mAuth.signInWithEmailAndPassword(usernameFromForm, passwordFromForm).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MovieListActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
